a13e724604e31e7247e5fdeb88d8ff1b7ea6f978
Kenediii <156183790+Kenediii@users.noreply.github.com>
2024-06-14 19:14:34 +0300

    fix

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,6 +1,7 @@
 import s from './ContactForm.module.css'
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import { nanoid } from "nanoid";
+import ReactInputMask from 'react-input-mask';
 import * as Yup from "yup";
 
 export const ContactForm = ({handleNewContact}) => {
@@ -25,17 +26,24 @@ export const ContactForm = ({handleNewContact}) => {
 
   return <div className={s.wrap}>
     <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={FeedbackSchema}>
-        <Form className={s.form_style}>
-            <label name='name'> Name
-                <Field label="name" name="name" className={s.fie_style} placeholder="name"/>
-                <ErrorMessage name='name'component="span" className={s.error_style}/>
-            </label>
-            <label name='number'> Number 
-                <Field name='number'  placeholder="number" className={s.fie_style}/>   
-                <ErrorMessage name='number' component="span" className={s.error_style}/>          
-            </label>
-            <button>ADD contact</button>
-        </Form>
+        {({setFieldValue}) => 
+                    <Form className={s.form_style}>
+                    <label name='name'> Name
+                        <Field label="name" name="name" className={s.fie_style} placeholder="name"/>
+                        <ErrorMessage name='name'component="span" className={s.error_style}/>
+                    </label>
+
+
+                    <label name='number'> Number 
+                        <Field name='number'  placeholder="number" className={s.fie_style}>
+                        {({ field }) => ( 
+                            <ReactInputMask {...field} mask="+3 (999) 999-99-99" maskChar="_" placeholder="+3 (___) ___-__-__" onChange={(e) => setFieldValue('number', e.target.value)} /> )}
+                        </Field>   
+                        <ErrorMessage name='number' component="span" className={s.error_style}/>          
+                    </label>
+                    <button>ADD contact</button>
+                </Form>
+        }
     </Formik>
 
   </div>
